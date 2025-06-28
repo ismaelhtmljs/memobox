d9435195a69c1c7b52dd63390d504c004e949629
ismaelhtmljs <107575663+ismaelhtmljs@users.noreply.github.com>
2025-06-28 14:36:30 -0500

    agregando el pop alert a memobox

--- src/app/todolist/page.tsx
+++ src/app/todolist/page.tsx
@@ -1,11 +1,13 @@
 import Headertodolist from "./components/header";
 import TodolistHome from "./main";
+import AlertPoP from "@/components/alerts/alert";
 import "@/app/todolist/css/global.css";
-import '@/app/todolist/css/darkmode/dark.css'
+import "@/app/todolist/css/darkmode/dark.css";
 
 export default function TodoListMain() {
   return (
     <>
+      <AlertPoP />
       <Headertodolist />
       <TodolistHome />
     </>
