adbddc749db4a96390366d7c0d8e591051d31e2b
ismaelhtmljs <107575663+ismaelhtmljs@users.noreply.github.com>
2025-07-11 20:37:33 -0500

    quitando el alertPopNotification #2

--- src/app/todolist/page.tsx
+++ src/app/todolist/page.tsx
@@ -1,13 +1,11 @@
 import Headertodolist from "./components/header/header";
 import TodolistHome from "./main";
-import AlertPoPUpdate from "@/components/alerts/updates/alertUpdate";
 import "@/app/todolist/css/global.css";
 import "@/app/todolist/css/darkmode/dark.css";
 
 export default function TodoListMain() {
   return (
     <>
-      <AlertPoPUpdate />
       <Headertodolist />
       <TodolistHome />
     </>
